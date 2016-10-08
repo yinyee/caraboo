95c40d22bc46444f3b96841eb79b78406d5db877
Matt Williams <matwilliams@hotmail.com>
2016-10-08 20:12:37 +0100

    Add DynamoDB support

--- src/com/github/yinyee/caraboo/User.java
+++ src/com/github/yinyee/caraboo/User.java
@@ -1,13 +1,22 @@
 package com.github.yinyee.caraboo;
 
-import javax.websocket.server.PathParam;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
 
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+
 @Path("/")
-@Produces(MediaType.APPLICATION_JSON)
 public class User {
 	public String username;
 	
@@ -16,10 +25,23 @@ public class User {
 	}
 	
 	@GET
-	@Path("/users/{user}")
-    public User getItem(@PathParam("user") String itemid) {
+	@Path("/users/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+    public User getItem(@PathParam("id") String userId) {
+		Map<String, AttributeValue> key = new HashMap<String,AttributeValue>();
+		key.put("id", new AttributeValue(userId));
+		Map<String, AttributeValue> value = Application.getDbClient().getItem(new GetItemRequest().withTableName("caraboo-users").withKey(key)).getItem();
+		// TODO use value
         return this;
     }
 
-
+	@PUT
+	@Path("/users/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+    public Response putItem(@PathParam("id") String userId, User user) {
+		Map<String, AttributeValue> item = new HashMap<String,AttributeValue>();
+		item.put("id", new AttributeValue(userId));
+		Application.getDbClient().putItem(new PutItemRequest().withTableName("caraboo-users").withItem(item));
+        return Response.noContent().build();
+    }
 }
